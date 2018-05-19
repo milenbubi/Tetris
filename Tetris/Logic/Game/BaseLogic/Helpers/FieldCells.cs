feb80622f596bb0a0851821299ec05f1932fc281
ffilip <milenbubi@gmail.com>
2018-05-19 19:34:25 +0300

    cosmetic changes

---
+++ Tetris/Logic/Game/BaseLogic/Helpers/FieldCells.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Tetris.Logic.Figures;
+using System.Collections.Generic;
+
+namespace Tetris.Logic.Game.BaseLogic.Helpers
+
+{
+    internal class FieldCells
+    {
+        private static int fieldRows;
+        private static int fieldColumns;
+        private static FigureElement horBorderElement;
+        private static FigureElement vertBorderElement;
+        private static List<FigureElement[]> fieldCells;
+
+        static FieldCells()
+        {
+            fieldRows = FieldData.GameFieldHeight;
+            fieldColumns = FieldData.GameFieldWidth;
+
+            fieldCells = new List<FigureElement[]>(fieldRows);
+
+            horBorderElement = new FigureElement(ConsoleColor.White, FieldData.HorizontalBorderSymbol);
+            vertBorderElement = new FigureElement(ConsoleColor.White, FieldData.VerticalBorderSymbol);
+
+            //Initialize the cell List
+            for (int row = 0; row < fieldRows - 1; row++)
+            {
+                fieldCells.Add(new FigureElement[fieldColumns]);
+                Initialize(row);
+            }
+
+            //Initialize the top and bottom
+            fieldCells.Insert(0, new FigureElement[fieldColumns]);
+            fieldCells.Add(new FigureElement[fieldColumns]);
+            InitializeHorizontalBorders();
+        }
+
+        internal FigureElement[] this[int index]
+        {
+            get
+            {
+                return fieldCells[index];
+            }
+        }
+
+        internal void Remove(int readyLine)
+        {
+            fieldCells.RemoveAt(readyLine);
+        }
+
+        internal void InsertNewRow()
+        {
+            fieldCells.Insert(1, new FigureElement[fieldColumns]);
+            Initialize(1);
+        }
+
+        internal void DrawAllRows()
+        {
+            DrawRowsInRange(0, fieldRows - 1);
+        }
+
+        internal void DrawRowsInRange(int from, int to)
+        {
+            for (int row = from; row <= to; row++)
+            {
+                Console.SetCursorPosition(0, row);
+                foreach (var item in fieldCells[row])
+                {
+                    Console.Write(string.Join("", item));
+                }
+            }
+        }
+
+        private static void Initialize(int row)
+        {
+            for (int col = 1; col < fieldColumns - 1; col++)
+            {
+                fieldCells[row][col] = new FigureElement(ConsoleColor.White, ' ');
+            }
+
+            //Vertical borders
+            fieldCells[row][0] = vertBorderElement;
+            fieldCells[row][fieldColumns - 1] = vertBorderElement;
+        }
+
+        private static void InitializeHorizontalBorders()
+        {
+            for (int col = 0; col < fieldColumns; col++)
+            {
+                fieldCells[0][col] = horBorderElement;
+                fieldCells[fieldRows - 1][col] = horBorderElement;
+            }
+        }
+    }
+}
---
+++ Tetris/Logic/Game/BaseLogic/Managers/GameInitializeManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Tetris.Logic.Game.BaseLogic.Helpers;
+
+namespace Tetris.Logic.Game.BaseLogic.Managers
+{
+    internal static class GameInitializeManager
+    {
+        private static FieldCells fieldCells;
+
+        static GameInitializeManager()
+        {
+            fieldCells = new FieldCells();
+        }
+
+        internal static void SetUpWindow()
+        {
+            int windowWidth = FieldData.GameFieldWidth + FieldData.InfoPanelWidth;
+
+            Console.BufferWidth = Console.WindowWidth = windowWidth;
+            Console.BufferHeight = Console.WindowHeight = FieldData.WindowHeight;
+            Console.Title = "TETRIS by ffilip";
+
+            DrawGameField();
+        }
+
+        internal static void ShowWelcomeMessage()
+        {
+            string[] words = { "Press", "any key", "to play !!!" };
+            PrintWelcomeMessage(words);
+
+            Console.ReadKey(true);
+
+            Console.ForegroundColor = Console.BackgroundColor;
+            PrintWelcomeMessage(words);
+        }
+
+        private static void DrawGameField()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            fieldCells.DrawAllRows();
+        }
+
+        private static void PrintWelcomeMessage(string[] words)
+        {
+            int messageRow = FieldData.WindowHeight / 2 - 2;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int wordLength = words[i].Length;
+                int messageCol = (FieldData.GameFieldWidth - wordLength) / 2;
+
+                Console.SetCursorPosition(messageCol, messageRow++);
+                Console.Write(words[i]);
+            }
+        }
+    }
+}
