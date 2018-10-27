85940c4bab7ec79bd5be42ecaff6de9c9a2b377a
milenbubi <milenbubi@abv.bg>
2018-10-27 15:25:24 +0300

    Deextend finish manager

--- Tetris/Logic/Game/GameController.cs
+++ Tetris/Logic/Game/GameController.cs
@@ -9,16 +9,31 @@ namespace Tetris.Logic.Game
 {
     internal static class GameController
     {
-        private static string[] finishMessage;
+        private static string newGameMessage;
+        private static string[] gameOverMessage;
+        private static string[] winMessage;
 
         static GameController()
         {
-            finishMessage = new string[]
-                {
-                    "GAME OVER\n",
-                    "Press 'Q' to quit game",
-                    "or press any key to play new game"
-                };
+            newGameMessage = "New game after {0} seconds";
+
+            gameOverMessage = new string[]
+            {
+                "GAME OVER\n",
+                "Press 'Q' to quit game",
+                "or press any key to play new game"
+            };
+
+            winMessage = new string[]
+            {
+                "  CONGRATULATION !!!\n",
+
+                "YOU WIN THE GAME !!!",
+                Environment.NewLine,
+                Environment.NewLine,
+                "  press any key to play new game\n",
+                "or 'Q' to quit game"
+            };
         }
 
         internal static Checker Check => Container.Checker;
@@ -75,7 +90,7 @@ namespace Tetris.Logic.Game
             {
                 for (int i = 3; i >= 0; i--)
                 {
-                    FinishManager.NewGameNotice(i);
+                    Print(new string[] { string.Format(newGameMessage, i) });
                     Delay(1000);
                 }
 
@@ -84,14 +99,23 @@ namespace Tetris.Logic.Game
 
             switch (GameData.status)
             {
-                case Status.GameOver:
-                    FinishManager.GameOverNotice(); break;
-                case Status.Win:
-                    FinishManager.WinnerNotice(); break;
+                case Status.GameOver: Print(gameOverMessage); break;
+                case Status.Win: Print(winMessage); break;
             }
 
             if (PressedKey == "Q")
                 FinishManager.EndOfGame();
+        }
+
+        private static void Print(string[] message)
+        {
+            Console.CursorTop = (FieldData.WindowHeight - message.Length) / 2;
+
+            foreach (var line in message)
+            {
+                Console.CursorLeft = (FieldData.WindowWidth - line.Length) / 2;
+                Console.WriteLine(line);
+            }
         }
     }
 }
